a218f1aed694c6d3bb2f578f6694623725f53af7
Ben Guericke <ben.guericke@gmail.com>
2019-09-25 20:07:37 -0600

    Finished Program 1

--- src/projects/project1/RadixSort.java
+++ src/projects/project1/RadixSort.java
@@ -6,14 +6,14 @@ class RadixSort{
     Comparable[] radixSort(Comparable[] randNums) {
         int places = getPlaces(randNums);
         int count = 0;
-        //Sorting
+        //gets each individual digit and puts in into tmp
         for (int p = 0; p < places; p++){
             Comparable[] tmp;
             tmp = new Comparable[randNums.length];
             for (int i = 0; i < randNums.length; i++) {
                 tmp[i] = (int) ((((int)randNums[i]  % (10 * Math.pow(10,count))) / Math.pow(10, count)));
             }
-
+            //Sorts by the given digit place i.e ones, tens, hundreds
             for (int i = 1; i < randNums.length; i++) {
                 for (int a = i; a > 0 && (tmp[a].compareTo(tmp[a-1]) > 0); a--){
                         Comparable placeHolder = tmp[a];
@@ -29,15 +29,17 @@ class RadixSort{
         return randNums;
     }
 
-
+    //Returns the the number of digits that the largest number in randNums
     private int getPlaces(Comparable[] randNums) {
         int max = 0;
         int places = 1;
+        //Finds max
         for (Comparable a: randNums) {
             if (max < (int)a){
                 max = (int)a;
             }
         }
+        //Finds digits in max
         while (max / 10 > 0) {
             max /= 10;
             places++;
